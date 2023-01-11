82f178201a2782392508b863560aee5cf23da086
mdilshod1994 <mdilshod1994@gmail.com>
2023-01-12 01:00:16 +0500

    added img svg animation for mob-version

--- src/js/animation-promo.js
+++ src/js/animation-promo.js
@@ -6,23 +6,46 @@ htmlRect.addEventListener('mouseenter', () => {
         htmlClassSvg.classList.remove('active')
     })
 })
+
 let cssRect = document.getElementById('css-rect')
 let cssClassSvg = document.querySelector('.css-svg')
 cssRect.addEventListener('mouseenter', () => {
-    console.log('asd', cssClassSvg);
     cssClassSvg.classList.add('active')
     cssRect.addEventListener('mouseleave', () => {
         cssClassSvg.classList.remove('active')
     })
 })
 
-
 let jsRect = document.getElementById('js-rect')
 let jsClassSvg = document.querySelector('.js-svg')
 jsRect.addEventListener('mouseenter', () => {
-    console.log('asd', jsClassSvg);
     jsClassSvg.classList.add('active')
     jsRect.addEventListener('mouseleave', () => {
         jsClassSvg.classList.remove('active')
     })
 })
+
+let windScreen = window.screen.width
+if (windScreen <= 760) {
+    slideSvg()
+}
+
+function slideSvg() {
+    let arr = [htmlClassSvg, cssClassSvg, jsClassSvg]
+    let currIndex = 0
+    setInterval(() => {
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+            if (index === currIndex) {
+                element.classList.add('active')
+            } else {
+                element.classList.remove('active')
+            }
+        }
+        if (currIndex >= arr.length - 1) {
+            currIndex = 0
+        } else {
+            currIndex++
+        }
+    }, 3000)
+}
